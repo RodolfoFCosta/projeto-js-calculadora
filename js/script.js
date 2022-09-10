a31f55ac35bd2bc37e173cb435d94d9b0696b1f9
Rodolfo Fialho da Costa <rodolfofialho905@gmail.com>
2022-09-10 16:38:12 -0300

    add lógica

--- js/script.js
+++ js/script.js
@@ -14,13 +14,117 @@ class Calculator {
     //add digit to calculator screen
     addDigit(digit) {
 
+        if (digit === "." && this.currentOperationText.innerText.includes(".")) {
+            return;
+        }
+
         this.currentOperation = digit
-        this.updateSren()
+        this.updateSreen()
+    }
+
+    //processar todos os operações da calculadora
+    processOperation(operation) {
+        // check curent is empty
+        if (this.currentOperationText.innerText === "" && operation !== "C") {
+            // change operation
+            if (this.previousOperationText.innerText !== "") {
+                this.changeOperation(operation);
+            }
+            return;
+        }
+
+        //get current and previous value
+        let operationValue
+        const previous = +this.previousOperationText.innerText.split(" ")[0];
+        const current = +this.currentOperationText.innerText;
+
+
+        switch (operation) {
+            case "+":
+                operationValue = previous + current;
+                this.updateSreen(operationValue, operation, current, previous);
+                break;
+            case "-":
+                operationValue = previous - current;
+                this.updateSreen(operationValue, operation, current, previous);
+                break;
+            case "/":
+                operationValue = previous / current;
+                this.updateSreen(operationValue, operation, current, previous);
+                break;
+            case "*":
+                operationValue = previous * current;
+                this.updateSreen(operationValue, operation, current, previous);
+                break;
+            case "DEL":
+                this.processDellOperation();
+                break;
+            case "CE":
+                this.processClearCurrentOperation();
+                break;
+            case "C":
+                this.processClearAllCurrent();
+                break;
+            case "=":
+                this.porcessEqualOperation();
+                break;
+            default:
+                return;
+        }
     }
 
     // chance values of the claculator screen
-    updateSren() {
-        this.currentOperationText.innerText += this.currentOperation;
+    updateSreen(
+        operationValue = null,
+        operation = null,
+        current = null,
+        previous = null
+    ) {
+
+        if (operationValue === null) {
+            this.currentOperationText.innerText += this.currentOperation;
+        } else {
+            // check if value is zero, if it is jut add current value
+            if (previous === 0) {
+                operationValue = current
+            }
+
+            // add curent value to previous 
+            this.previousOperationText.innerText = `${operationValue} ${operation}`
+            this.currentOperationText.innerText = "";
+        }
+    }
+
+    // change math operation
+    changeOperation(operation) {
+
+        const mathOperation = ["*", "/", "+", "-"]
+
+        if (!mathOperation.includes(operation)) {
+            return
+        }
+
+        this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation;
+    }
+
+    // deletar um digito
+    processDellOperation() {
+        this.currentOperationText.innerText = this.currentOperationText.innerText.slice(0, -1);
+    }
+    // deletar toda operaçao
+    processClearCurrentOperation() {
+        this.currentOperationText.innerText = "";
+    }
+
+    processClearAllCurrent() {
+        this.currentOperationText.innerText = "";
+        this.previousOperationText.innerText = "";
+    }
+
+    porcessEqualOperation() {
+        const operation = previousOperationText.innerText.split(" ")[1]
+        this.processOperation(operation);
+
     }
 }
 
@@ -37,7 +141,7 @@ buttons.forEach((btn) => {
 
         } else {
 
-            console.log('op' + value)
+            calc.processOperation(value)
 
         }
     });
